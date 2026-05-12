8a310e60f87bb5d30eeb0ba8cf83fe8760ba6669
Diba Mirza <diba@169-231-117-58.wireless.ucsb.edu>
2026-05-12 12:19:20 -0700

    lect 10

--- lect10/lect10.py
+++ lect10/lect10.py
@@ -8,6 +8,16 @@
 #   4. Use PIL to open image files
 
 # Get the Pokemon data set
+import os
+import kagglehub
+from PIL import Image
+
+# Download latest version
+path = kagglehub.dataset_download("vishalsubbiah/pokemon-images-and-types")
+
+print("Path to dataset files:", path)
+csv_file = os.path.join(path, "pokemon.csv")
+images_dir = os.path.join(path, "images")
 
 
 # ============================================================
@@ -17,21 +27,27 @@
 # Step 1: manual open / read / close. 
 # Read the first 200 characters in the file
 
+f = open(csv_file)
+contents = f.read()
+print(contents[:200])
+f.close()
 
 # Step 2: with — closes automatically, even if an error occurs
-# with open(csv_file) as f:
-#     for line in f:
-#         print(line, end="")
-
 # TODO: use with + for line in f to print the first 5 lines
-
+print("\n\nOpen safely with the keyword with\n")
+with open(csv_file) as f: # Simply replacing the open and close operations
+    for i, line in enumerate(f):
+        if i == 5:
+            break
+        print(line, end="")
+  
 
 # ============================================================
 # PART 2: Parsing a CSV line
 # ============================================================
 # Each line looks like:  "bulbasaur,Grass,Poison,ivysaur\n"
-# line.strip()           ->  
-# line.strip().split(',') -> 
+# line.strip()           ->   "bulbasaur,Grass,Poison,ivysaur"
+# line.strip().split(',') ->  ['bulbasaur', 'Grass', 'Poison', 'ivysaur']
 
 # Concept Test — what does line.strip().split(',') return?
 # A. ['bulbasaur,Grass,Poison,ivysaur']
@@ -45,15 +61,38 @@
 # ============================================================
 # Goal: type -> [list of pokemon names]
 # {'Grass': ['bulbasaur', 'ivysaur', ...], 'Fire': ['charmander', ...], ...}
+# {}
 
-def build_type_dict(csv_file):
+def build_pokemon_dict(csv_file):
     '''Return a dict mapping Type1 -> [list of pokemon names].'''
-    type_dict = {}
-    return type_dict
+    pokemon_dict = {} 
+    # Open the file
+    with open(csv_file) as f:
+        # Read the header (one line) -- skip the header
+        f.readline()
+        # For every line in the file
+        for line in f:
+            # line = "bulbasaur,Grass,Poison,ivysaur\n"
+            # strip the new line and split line into 4 parts
+            # we need to get the name of the pokemon (index 0)
+            # we need to get the type of the pokemon (index 1)
+            parts = line.strip().split(",") # parts is a list with 4 elements
+            # e.g. name = "bulbasaur"
+            #      poke_type  = "Grass"
+            name = parts[0]
+            poke_type = parts[1]
+            # pokemon_dict  may or may not be empty
+            # assumes pokemon_dict is empty
+            # {}  ---> {"Grass": ["bulbasaur"] }
+            if poke_type not in pokemon_dict:
+                pokemon_dict[poke_type] = []
+            pokemon_dict[poke_type].append(name)
+    return pokemon_dict
 
 # TODO: call build_type_dict, print the keys, print all Fire-type pokemon
-
-
+poke_dict = build_pokemon_dict(csv_file)
+#print(poke_dict['Grass'][0:8])
+print(list(poke_dict.keys()))
 # ============================================================
 # PART 4: Opening an image file with PIL
 # ============================================================
@@ -63,18 +102,26 @@ def show_pokemon(name, images_dir):
     '''Open and display the image for the given pokemon.'''
     filepath = os.path.join(images_dir, name + ".png")
     # TODO: open with Image.open() and call img.show()
+    img  = Image.open(filepath)
+    img.show()
     pass
 
 # TODO: show_pokemon("pikachu", images_dir)
-
+# show_pokemon("pikachu", images_dir)
 
 # ============================================================
 # PART 5: Explore the data  [if time permits]
 # ============================================================
+import random
 
+def show_random_pokemon_per_type(poke_dict, images_dir):
+    '''Show one random pokemon for each type'''
+    for type_name, pokemon_list in poke_dict.items():
+        name = random.choice(pokemon_list)
+        print(f"{type_name} : {name}")
+        show_pokemon(name, images_dir)
 
-
-
+show_random_pokemon_per_type(poke_dict, images_dir)
 # ============================================================
 # PRACTICE
 # ============================================================
